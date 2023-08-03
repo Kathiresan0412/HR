4c03b5a2aa6add2db58a2456f0661c377f4c31fc
Dastanvishwa <104714592+Dastanvishwa@users.noreply.github.com>
2023-08-03 15:21:59 +0530

    main

--- database/migrations/2023_08_03_071406_create_employee_documents_table.php
+++ database/migrations/2023_08_03_071406_create_employee_documents_table.php
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('employee_documents');
+        Schema::dropIfExists('employee_documents'); //jjj
     }
 };
