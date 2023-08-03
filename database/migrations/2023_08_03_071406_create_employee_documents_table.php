e0f4ee29774d7b10ee0109233a6c7da0417582f1
Dastanvishwa <104714592+Dastanvishwa@users.noreply.github.com>
2023-08-03 14:56:25 +0530

    aa

--- database/migrations/2023_08_03_071406_create_employee_documents_table.php
+++ database/migrations/2023_08_03_071406_create_employee_documents_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->id();
             $table->bigInteger('employee')->unsigned()->nullable();
             $table->foreign('employee')->references('id')->on('employees')->onDelete('set null');
-            $table->string('ol_level_al_level_resheets');//all file upload
-            $table->string('feedback_date');
+            $table->string('ol_level_al_level_resheets'); //all file upload
+            $table->string('aa');
             $table->string('feedback_date');
             $table->string('feedback_date');
             $table->timestamps();
